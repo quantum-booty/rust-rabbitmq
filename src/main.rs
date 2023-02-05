bd8a0998b140589b6e3b185de135eaa45e31c2c9
Henry Wang <henrywang97@gmail.com>
2023-02-05 21:42:18 +0000

    fix comment

--- src/main.rs
+++ src/main.rs
@@ -79,10 +79,13 @@ async fn main() -> Result<()> {
 }
 
 fn set_up_logging() {
-    // Parse an `EnvFilter` configuration from the `RUST_LOG` environment variable.
     let filter = EnvFilter::builder()
         .with_default_directive(LevelFilter::INFO.into())
         .from_env_lossy();
+    // Alternatively can parse an `EnvFilter` configuration from the `RUST_LOG` environment variable
+    // e.g. by setting an environment variable RUST_LOG=debug
+    // EnvFilter::from_default_env()
+
     // construct a subscriber that prints formatted traces to stdout
     tracing_subscriber::registry()
         .with(fmt::layer())
